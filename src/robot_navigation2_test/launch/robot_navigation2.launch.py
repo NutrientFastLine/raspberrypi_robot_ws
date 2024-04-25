9c81ee62b45e302b5a99a21a991d135cf18231c1
feiyu_raspberrypi_2 <Mr.zhu345@gmail.com>
2024-04-25 21:46:21 +0800

    nav2 test 2.1 success

--- src/robot_navigation2_test/launch/robot_navigation2.launch.py
+++ src/robot_navigation2_test/launch/robot_navigation2.launch.py
@@ -12,8 +12,15 @@ def generate_launch_description():
 
     robot_navigation2_dir = get_package_share_directory('robot_navigation2_test')
 
-    nav2_amcl_yaml = os.path.join(robot_navigation2_dir,'param','robot_nav2_amcl.yaml')
+    nav2_amcl_yaml        = os.path.join(robot_navigation2_dir,'param','nav2_amcl.yaml')
+    nav2_behavior_yaml    = os.path.join(robot_navigation2_dir,'param','nav2_behavior.yaml')
+    nav2_btnavigator_yaml = os.path.join(robot_navigation2_dir,'param','nav2_btnavigator.yaml')
+    nav2_controller_yaml  = os.path.join(robot_navigation2_dir,'param','nav2_controller.yaml')
+    nav2_planner_yaml     = os.path.join(robot_navigation2_dir,'param','nav2_planner.yaml')
+    nav2_waypointfollower_yaml     = os.path.join(robot_navigation2_dir,'param','nav2_waypointfollower.yaml')
+
     map_yaml_path = LaunchConfiguration('map',default=os.path.join(robot_navigation2_dir,'maps','test1_map.yaml'))
+
     rviz_config_dir = os.path.join(robot_navigation2_dir,'rviz2','robot_nav2_test.rviz')
 
     amcl_node =  Node(
@@ -32,14 +39,33 @@ def generate_launch_description():
                         {'yaml_filename':map_yaml_path} 
                        ])
     
-    rviz_node =  Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            output='screen')
+    behavior_node =  Node(
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='behavior_server',
+            output='screen',
+            parameters=[nav2_behavior_yaml])
     
-    lifecycle_node =  Node(
+    btnavigator_node =  Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[nav2_btnavigator_yaml])
+    controller_node =  Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[nav2_controller_yaml])
+    planner_node =  Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[nav2_planner_yaml])
+    
+    lifecycle_mapper_node =  Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_mapper',
@@ -47,17 +73,39 @@ def generate_launch_description():
                         {'autostart':True},
                         {'node_names':['map_server',
                                        'amcl']}],
-        #     parameters=[{'use_sim_time': False},
-        #                 {'autostart':True},
-        #                 {'node_names':['map_server']}],
+            output='screen')
+    
+    lifecycle_nav2_node =  Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_nav2',
+            parameters=[{'use_sim_time': False},
+                        {'autostart':True},
+                        {'node_names':['planner_server',
+                                       'controller_server',
+                                       'behavior_server',
+                                       'bt_navigator']}],
+            output='screen')
+
+    rviz_node =  Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
             output='screen')
     
     ld = LaunchDescription()
     ld.add_action(map_node)
     ld.add_action(amcl_node)
-#     ld.add_action(rviz_node) 
-    ld.add_action(lifecycle_node)
+    ld.add_action(behavior_node)
+    ld.add_action(btnavigator_node)
+    ld.add_action(controller_node)
+    ld.add_action(planner_node)
 
+    ld.add_action(lifecycle_mapper_node)
+    ld.add_action(lifecycle_nav2_node)
+
+#     ld.add_action(rviz_node) 
 
     return ld
 
